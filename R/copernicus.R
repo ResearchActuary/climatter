44415ccf3eabb51625478eb5c4410cc25db4bc03
Brian Fannin <bfannin@casact.org>
2020-08-04 14:33:46 -0400

    Shift to package structure

---
+++ R/copernicus.R
@@ -0,0 +1,78 @@
+#' download_one_file
+#' 
+#' Make a single request to Copernicus. 
+#' 
+#' @param user_name The user name for the service
+#' @param date_in The date associated with the data
+#' @param variables_in String vector of values to retrieve
+#' @param save_path Path where the data should be saved. The default is a folder called "data"
+#' 
+#' @return The function will return the name of the downloaded file, as a string
+#' 
+#' @details 
+#' 
+#' @importFrom ecmwfr wf_request
+#' @importFrom lubridate day
+#' @importFrom lubridate month
+#' @importFrom lubridate year
+#' @export
+download_one_file <- function(user_name, date_in, variables_in, save_path = "data") {
+  
+  times = sprintf("%02i", 1:24 - 1)
+  times = paste0(times, ":00")
+  
+  my_request <- list(
+    dataset_short_name = "reanalysis-era5-land"
+    , variable = variables_in
+    , year = lubridate::year(date_in)
+    , month = lubridate::month(date_in)
+    , day = lubridate::day(date_in)
+    , time = times
+    , area = c(90, -172, 24, -52)
+    , format = "netcdf"
+    , target = paste(date_in, "era5.nc", sep = "-")
+  )
+  
+  wf_request(
+    user = user_name
+    , request = my_request   
+    , transfer = TRUE  
+    , path = save_path
+    , verbose = FALSE
+  )
+}
+
+#' extract_one_file
+#' 
+#' @details This does little apart from converting the contents of an NC file into a data frame. Note that this object will likely be very large, on the order of more than one million rows. 
+#' 
+#' If a column called 'time' exists in the data frame, it will be renamed 'cds_time'. This column name is expected in the function `add_date_time()`.
+#' 
+#' @param file_in The name of an NC file from which to extract
+#' @param delete_source Boolean indicating whether to delete the file once the extract has completed
+#' 
+#' @return A tibble (data frame) of the contents
+#' 
+#' @importFrom tidync tidync
+#' @importFrom magrittr %>%
+#' @importFrom dplyr rename
+#' 
+#' @export
+extract_one_file <- function(file_in, delete_source = TRUE) {
+  
+  tbl_out <- file_in %>% 
+    tidync() %>% 
+    hyper_tibble()
+  
+  if ('time' %in% names(tbl_out)) {
+    tbl_out <- tbl_out %>% 
+      rename(cds_time = time)
+  }
+  
+  if (delete_source) {
+    unlink(file_in)
+  }
+  
+  tbl_out
+}
+
---
+++ R/wrangle.R
@@ -0,0 +1,82 @@
+#' trim_grid
+#' 
+#' @details 
+#' 
+#' 
+#' @param tbl_in Data frame of climate data
+#' @param lat_gran Granularity of latitude
+#' @param lon_gran Granularity of longitude
+#' 
+#' @importFrom magrittr %>% 
+#' @importFrom dplyr filter
+#' 
+#' @export
+trim_grid <- function(tbl_in, lat_gran = .05, lon_gran = .05) {
+  tbl_in %>% 
+    filter(
+      abs(longitude - round(longitude)) < lat_gran
+      , abs(latitude - round(latitude)) < lon_gran
+    )
+}
+
+cds_to_datetime <- function(x){
+  
+  x %>% 
+    magrittr::multiply_by(60) %>%
+    magrittr::multiply_by(60) %>%
+    as.POSIXct(origin = "1900-01-01 00:00:00")
+}
+
+add_date_time <- function(tbl_in) {
+  
+  tbl_in %>% 
+    mutate(
+      date_time = cds_to_datetime(cds_time)
+      , date = as.Date(date_time)
+    )
+}
+
+#' summarize_day
+#' 
+#' @param tbl_in Data frame of climate data to summarize
+#' 
+#' @return A data frame
+#' 
+#' @importFrom dplyr group_by
+#' @importFrom dplyr summarise_all
+#' @importFrom dplyr ungroup
+#' @importFrom magrittr %>% 
+#' 
+#' @export
+summarize_day <- function(tbl_in) {
+  
+  keep_cols <- setdiff(names(tbl_in), c('date_time', 'cds_time'))
+  
+  tbl_in <- tbl_in[, keep_cols]
+  
+  tbl_in %>% 
+    group_by(longitude, latitude, date) %>% 
+    summarise_all(
+      .funs = list(mean = mean, median = median, min = min, max = max)
+      , na.rm = TRUE
+    ) %>% 
+    ungroup()
+  
+}
+
+create_tracker_table <- function(months = 1:12, days = 1:31, years = 1981:2020) {
+  
+  tbl_requests <- crossing(
+    month = months
+    , day = days
+    , year = years
+  ) %>% 
+    mutate(
+      date = lubridate::make_date(year, month, day)
+    ) %>% 
+    filter(
+      !is.na(date)
+    ) %>% 
+    select(-year, -month, -day)
+  
+}
